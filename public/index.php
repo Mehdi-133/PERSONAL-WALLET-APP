aba45ef26f770817221bd5afc4e14db3a40c604c
Mehdi-133 <mehdikarbitou@gmail.com>
2026-01-11 18:47:48 +0100

    readme file

--- public/index.php
+++ public/index.php
@@ -1,17 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/config/database.php';
-// require_once __DIR__ . '/../app/Modals/User.php';
 
 use App\Core\handelErrors;
 use App\Core\Database;
-use App\Core\Security;
-use App\Controllers\WalletController;
-use App\Controllers\AuthController;
-use App\Modals;
 use App\Core\Router;
-
-    
 
 handelErrors::register();
 
